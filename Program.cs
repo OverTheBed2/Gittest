0c8c9bcd5a6574c891be2f2d5f4a64c4b816a567
Cameron <cam.meylan@gmail.com>
2020-05-05 16:50:33 +0100

    Book Code
    Is book code

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Andre_x2___under_x1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books books = new Books();
+            Console.WriteLine("What is the name of the book?");
+
+            books.name = Console.ReadLine();
+
+            Console.WriteLine("When was the release date?");
+
+            books.date = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many pages are in the book?");
+
+            books.pages = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is the name of the Author?");
+
+            books.authour = Console.ReadLine();
+
+            Console.WriteLine("The book is", books.name);
+            Console.WriteLine("The Date it came out was", books.date);
+            Console.WriteLine("The Amount of pages are", books.pages);
+            Console.WriteLine("The authour is", books.authour);
+        ;
+        }
+    }
+}
